abddb924187ebd3cee66effd402154064146523a
Ardhianto Raharjo <95130519+ardhi-dhito@users.noreply.github.com>
2025-03-19 11:39:52 +0700

    feat: implement repeatString function and enable test cases

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(inputString, repeatCount) {
+    let repeatedString = '';
 
+    if (repeatCount < 0) {
+        return 'ERROR';
+    } else {
+        for(i = 0; i < repeatCount; i++) {
+            repeatedString = repeatedString + inputString;
+        }
+
+        return repeatedString;
+    }
 };
 
 // Do not edit below this line
