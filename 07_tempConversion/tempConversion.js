fb3ffcccd40207e5c12b5e9b03d33175805d0e2c
Ardhianto Raharjo <95130519+ardhi-dhito@users.noreply.github.com>
2025-03-20 10:13:53 +0700

    feat: implement convertToCelcius and convertToFahrenheit functions and enable test cases

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(a) {
+  let tempInCelsius = (a - 32) * (5/9);
+  return Number( tempInCelsius.toFixed(1) );
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(b) {
+  let tempInFahrenheit = (b * 9 / 5) + 32;
+  return Number( tempInFahrenheit.toFixed(1) );
 };
 
 // Do not edit below this line
